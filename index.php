efaeacc22d46f11185a5a9e33146e101db178c6e
Ryder Dettloff <dettloff.ryder@student.greenriver.edu>
2024-02-03 15:03:19 -0500

    tried to change some things, struggling with getting routing to work for the apply button

--- index.php
+++ index.php
@@ -5,39 +5,31 @@
 * This is my Controller
 */
 
-//Display Errors
+// Display Errors
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require the autoload file
+// Require the autoload file
+require_once('vendor/autoload.php');
 
-require_once ('vendor/autoload.php');
-//instance method
+// Instance method
 $f3 = Base::instance();
 
-//Define a default route
-$f3->route('GET /', function() {
-    //echo "Pet Home";
+// Define a default route
+$f3->route('GET /', function () {
+    // Load the home view
     $view = new Template();
     echo $view->render('views/home.html');
 });
-$f3->route('GET /experience', function() {
-    $experience = new Template();
-    echo $experience->render('views/experience.html');
+
+
+$f3->route('GET /apply-now', function () { //confusion with my rerouting
+    //getting a 404 error. routing with the apply now button is causing an issue.
+
+    $newView = new Template();
+    echo $newView->render('views/personal.php');
 });
 
-$f3->route('GET /personal', function() {
-    $view = new Template();
-    echo $view->render('views/personal.html');
-});
-$f3->route('GET /summary', function() {
-    $view = new Template();
-    echo $view->render('views/summary.html');
-});
-
-$f3->route('GET /jobOpenings', function() {
-    $view = new Template();
-    echo $view->render('views/jobOpenings.html');
-});
-//run fat-free
+// Run Fat-Free
 $f3->run();
+?>

302a8b40aa60221b6341cb61ae7370f06b7d8015
Ryder Dettloff <dettloff.ryder@student.greenriver.edu>
2024-02-03 03:15:33 -0500

    trying to get controller to work

--- index.php
+++ index.php
@@ -18,8 +18,26 @@ $f3 = Base::instance();
 //Define a default route
 $f3->route('GET /', function() {
     //echo "Pet Home";
-    $view= new Template();
+    $view = new Template();
     echo $view->render('views/home.html');
+});
+$f3->route('GET /experience', function() {
+    $experience = new Template();
+    echo $experience->render('views/experience.html');
+});
+
+$f3->route('GET /personal', function() {
+    $view = new Template();
+    echo $view->render('views/personal.html');
+});
+$f3->route('GET /summary', function() {
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+$f3->route('GET /jobOpenings', function() {
+    $view = new Template();
+    echo $view->render('views/jobOpenings.html');
 });
 //run fat-free
 $f3->run();

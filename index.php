4a8f3c1f805cdd7c2c56e193c4bf565f388fcb8b
Ryder Dettloff <dettloff.ryder@student.greenriver.edu>
2024-03-03 22:44:16 -0500

    updated index page to reflect controller as a global

--- index.php
+++ index.php
@@ -18,137 +18,28 @@ require_once('model/DataLayer.php');
 $f3 = Base::instance();
 
 
+$controller = new Controller($f3);
+
 // Define a default route
-$f3->route('GET /', function () {
-    // Load the home view
-    $view = new Template();
-    echo $view->render('views/home.html');
+$f3->route('GET /', function () use ($controller) {
+    $controller->home();
 });
 
-
-$f3->route('GET|POST /personal', function ($f3) {
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $firstName = '';
-        $lastName = '';
-        $email = '';
-        $state = '';
-        $phoneNumber = '';
-
-        if (model::validFirstName($_POST['firstName'])) {
-            $firstName = $_POST["firstName"];
-        } else {
-            $f3->set('errors["firstName"]', 'invalid firstname');
-        }
-
-        if (model::validLastName($_POST['lastName'])) {
-            $lastName = $_POST["lastName"];
-        } else {
-            $f3->set('errors["lastName"]', 'invalid lastName');
-        }
-
-        if (model::validEmail($_POST['email'])) {
-            $email = $_POST["email"];
-        } else {
-            $f3->set('errors["email"]', 'invalid email');
-        }
-
-        if (model::validPhone($_POST['phoneNumber'])) {
-            $phoneNumber = $_POST["phoneNumber"];
-        } else {
-            $f3->set('errors["phoneNumber"]', 'invalid phone Number');
-        }
-
-        if (empty($f3->get('errors'))) {
-            $state = $_POST['state'];
-            $f3->set('SESSION.firstName', $firstName);
-            $f3->set('SESSION.lastName', $lastName);
-            $f3->set('SESSION.email', $email);
-            $f3->set('SESSION.state', $state);
-            $f3->set('SESSION.phoneNumber', $phoneNumber);
-
-            $f3->reroute('experience');
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/personal.html');
+$f3->route('GET|POST /personal', function ($f3){
+    $GLOBALS['controller']->personal();
 });
-$f3->route('GET|POST /experience', function ($f3) {
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $biography = '';
-        $githubLink = '';
-        $yearsOfExperience = '';
-        $relocate = '';
-
-        if (isset($_POST['githubLink']) and model::validGithub($_POST['githubLink'])) {
-            $githubLink = $_POST['githubLink'];
-        } else {
-            $f3->set('errors["githubLink"]', "Invalid Github Link");
-        }
-
-        if (isset($_POST['yearsOfExperience']) and model::validExperience($_POST['yearsOfExperience'])) {
-            $yearsOfExperience = $_POST['yearsOfExperience'];
-        } else {
-            $f3->set('errors["yearsOfExperience"]', "Please select years of experience");
-        }
-
-        if (empty($f3->get('errors'))) {
-            $biography = $_POST['biography'];
-            $relocate = $_POST['relocate'];
-            $f3->set('SESSION.biography', $biography);
-            $f3->set('SESSION.githubLink', $githubLink);
-            $f3->set('SESSION.yearsOfExperience', $yearsOfExperience);
-            $f3->set('SESSION.relocate', $relocate);
-
-            $f3->reroute('jobOpenings');
-        }
-    }
-
-    $f3->set('yearsOfExperience', DataLayer::getExperience());
-
-    $newView = new Template();
-    echo $newView->render('views/experience.html');
-});
+$f3->route('GET|POST /experience', function ($f3){
+    $GLOBALS['controller']->experience();});
 
 $f3->route('GET|POST /jobOpenings', function ($f3) {
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $GLOBALS['controller']->jobOpenings();});
 
-        if (isset($_POST['softwareDevelopmentJobs'])) {
-            $softwareDevelopmentJobs = implode(', ', $_POST['softwareDevelopmentJobs']);
-        }else{
-            $softwareDevelopmentJobs = "None Selected";
-        }
-        if (isset($_POST['industryVerticals'])) {
-            $industryVerticals = implode(', ', $_POST['industryVerticals']);
-        }else{
-            $industryVerticals = "None Selected";
-        }
+$f3->route('GET|POST /summary', function ($f3){
+    $GLOBALS['controller']->summary();});
 
-
-        $f3->set('SESSION.softwareDevelopmentJobs', $softwareDevelopmentJobs);
-        $f3->set('SESSION.industryVerticals', $industryVerticals);
-
-        $f3->reroute('summary');
-    }
-    $newView = new Template();
-    echo $newView->render('views/jobOpenings.html');
-});
-$f3->route('GET|POST /summary', function () {
-
-    $newView = new Template();
-    echo $newView->render('views/summary.html');
-});
-$f3->route('GET /home', function () {
-    // Load the home view
-    $newView = new Template();
-    echo $newView->render('views/home.html');
-});
-
+$f3->route('GET /home', function ($f3){
+    $GLOBALS['controller']->home();});
 
 // Run Fat-Free
-$f3->run();
-?>
+$f3->run(); //instance method
